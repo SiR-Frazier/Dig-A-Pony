a01b66dcb29ce9c788ccb17d90da965e0d929941
Bryce Frazier <bx.frazier@gmail.com>
2018-03-30 11:51:07 -0700

    add calendar component and event.model constructor

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Event } from '../event.model';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-export class CalendarComponent implements OnInit {
+export class CalendarComponent {
+  events: Event[] = [
+    new Event("4/1-Happy Hour w/ Maxx Bass", "FUNK/ BOOGIE/ RAP/ R&B")
 
+  ];
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
