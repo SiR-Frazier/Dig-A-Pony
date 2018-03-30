3b685c9658dc57f1e887b4871cd72ae9e8c147a5
Bryce Frazier <bx.frazier@gmail.com>
2018-03-30 10:49:39 -0700

    add constructor for menu items. items are visible, but it does not look pretty

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,7 +7,12 @@ import { Item } from '../item.model';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-
+  items: Item[] = [
+    new Item("SMOKESTACK LIGHTING", "HAVANA CLUB, TOWNSHEND'S SPICED TEA LIQUEUR, VANILLA, HONEY, LEMON, HOT", 9),
+    new Item("96 TEARS","GINGER VODKA, LIME, SUGAR, BITTERS, GINGER BEER, PINT", 8),
+    new Item("FROZEN PEACH-CINNAMON MARGARITA","BLANCO TEQUILA, PEACH NECTAR, LIME, ORANGE, CINNAMON SYRUP, TRIPLE SEC, FROZEN", 9),
+    new Item ("THE PINK MOOSE","SPICY TEQUILA, FRESH LIME, GRAPEFRUIT, AGAVE, PEYCHAUD'S BITTERS, ROCKS", 8)
+  ];
   constructor() { }
 
 }
