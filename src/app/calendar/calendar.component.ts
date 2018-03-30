8a79af041557a75a93cc6adaa3722a595de77daf
Bryce Frazier <bx.frazier@gmail.com>
2018-03-30 13:19:17 -0700

    hard code several calendar events in calendar.component file

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -8,8 +8,15 @@ import { Event } from '../event.model';
 })
 export class CalendarComponent {
   events: Event[] = [
-    new Event("4/1-Happy Hour w/ Maxx Bass", "FUNK/ BOOGIE/ RAP/ R&B")
-
+    new Event("4/1-Happy Hour w/ Maxx Bass", "FUNK/ BOOGIE/ RAP/ R&B"),
+    new Event("4/2- ANJALI & THE INCREDIBLE KID", "VINTAGE INTERNATIONAL"),
+    new Event("4/3-GWIZSKI", "BOOGIE & MODERN FUNK"),
+    new Event("4/4-TIMOTHY B", "GOLDEN ERA HIPHOP"),
+    new Event("4/5-SWEATY TECHNIQUE", "SOUL & FUNK"),
+    new Event("4/6-SQUINTSY JONES", "MID CENTURY JAZZ"),
+    new Event("4/7-MAXAMILLION", "SOUL/ RAP/ SWEAT"),
+    new Event("4/8-LAMAR", "BOOGIE/HIPHOP/R&B/ELECTRO"),
+    new Event("4/9-JIMBO", "TRAP/HIPHOP")
   ];
   constructor() { }
 
