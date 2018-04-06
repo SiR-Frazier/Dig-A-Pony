d6ec5e0f3bbc1dadf93d6bf13b405a91b3ca1430
Bryce Frazier <bx.frazier@gmail.com>
2018-04-06 09:29:48 -0700

    udate calendar list for the entire month

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -16,7 +16,29 @@ export class CalendarComponent {
     new Event("4/6-SQUINTSY JONES", "MID CENTURY JAZZ"),
     new Event("4/7-MAXAMILLION", "SOUL/ RAP/ SWEAT"),
     new Event("4/8-LAMAR", "BOOGIE/HIPHOP/R&B/ELECTRO"),
-    new Event("4/9-JIMBO", "TRAP/HIPHOP")
+    new Event("4/9-JIMBO", "TRAP/HIPHOP"),
+    new Event("4/10-BAD WIZARD","50S & 60S SOUL & ROCK"),
+    new Event("4/11 - THE SPINS! VINYL LISTENING PARTY FOR ARCTIC MONKEY'S 'WHATEVER PEOPLE SAY I AM, THATS WHAT I'M NOT'","Indie Rock"),
+    new Event("4/11 - MARTI","WHATEVER SHE WANTS, NO ONE DOES IT BETTER"),
+    new Event("4/12 - GWIZSKI","BOOGIE & MODERN FUNK"),
+    new Event("4/13 - MAXX BASS & FREAKY OUTTY","FUNK/ BOOGIE/ RAP/ R&B"),
+    new Event("4/14 - MONTEL SPINOZZA","THE NOISE, THE FUNK"),
+    new Event("4/15 - MIENNE & LOVELOST","TROPICAL RIDDIM/DANCEHALL/ DEEP HOUSE"),
+    new Event("4/16 - THE AMAZING STUMPTOWN SPELLING BEE","7PM"),
+    new Event("4/17 - NOCHES LATINAS W/ DJ BLAS","SALSA/ MERENGUE/REGGAETON"),
+    new Event("4/18 - HONEST JOHN","SOUL & GARAGE"),
+    new Event("4/19 - PAPI FIMBRES","AFRO FUNK & LATIN PSYCH"),
+    new Event("4/20 - BATTLES & LAMAR","BOOGIE/ HIP HOP/ R&B/ ELECTRO"),
+    new Event("4/21 - MAXAMILLION","SOUL/ RAP/ SWEAT"),
+    new Event("4/22 - EMERSON","HIP HOP & R&B OF THE EARLY AUGHTS"),
+    new Event("4/24 - AM GOLD","GREAZY OLDIES"),
+    new Event("4/25 - ATOM 13","KITCHEN SINK OF SONIC EXCELLENCE"),
+    new Event("4/26 - A TRAIN & EAGLE SUN KING","VINTAGE CUMBIA"),
+    new Event("4/27 - JIMBO","FUNK/ RAP/ R&B"),
+    new Event("4/28 - FREAKY OUTTY & MAXX BASS","FLOOR FILLERS"),
+    new Event("4/29 - DO RIGHT SUNDAY","THROWBACK HIP HOP & R&B"),
+    new Event("4/30 - YAWNING","SOUL & FUNK")
+
   ];
   // constructor() { }
 
